7c4557dab477de6083bc6543d20f9a1166120f45
Frank Chen <hfchen1006@gmail.com>
2026-07-06 20:49:41 -0700

    Added clear button and auto update the results

--- script.js
+++ script.js
@@ -5,22 +5,40 @@ const priceInput = document.getElementById("price");
 const extraInput = document.getElementById("extraLunches");
 
 const calculateBtn = document.getElementById("calculateBtn");
+const clearBtn = document.getElementById("clearBtn");
 
 const totalLunchesResult = document.getElementById("totalLunches");
 const totalPriceResult = document.getElementById("totalPrice");
 
+const inputs = [seniorInput, juniorInput, teacherInput, priceInput, extraInput];
+
 function calculateLunches() {
-    const seniorStudents = Number(seniorInput.value) || 0;
-    const juniorStudents = Number(juniorInput.value) || 0;
-    const teachers = Number(teacherInput.value) || 0;
-    const price = Number(priceInput.value) || 0;
-    const extraLunches = Number(extraInput.value) || 0;
+  const seniorStudents = Number(seniorInput.value) || 0;
+  const juniorStudents = Number(juniorInput.value) || 0;
+  const teachers = Number(teacherInput.value) || 0;
+  const price = Number(priceInput.value) || 0;
+  const extraLunches = Number(extraInput.value) || 0;
 
-    const totalLunches = seniorStudents + juniorStudents + teachers + extraLunches;
-    const totalPrice = totalLunches * price;
+  const totalLunches =
+    seniorStudents + juniorStudents + teachers + extraLunches;
+  const totalPrice = totalLunches * price;
 
-    totalLunchesResult.textContent = totalLunches;
-    totalPriceResult.textContent = totalPrice.toLocaleString();
+  totalLunchesResult.textContent = totalLunches;
+  totalPriceResult.textContent = totalPrice.toLocaleString();
 }
 
+function clearForm() {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+
+  calculateLunches();
+}
+ 
+
 calculateBtn.addEventListener("click", calculateLunches);
+clearBtn.addEventListener("click", clearForm);
+
+inputs.forEach((input) => {
+  input.addEventListener("input", calculateLunches);
+});
